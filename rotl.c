130eb6d20f2efbc712ac38a96f1cc263eac05a78
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2022-12-17 20:25:08 +0100

    new changes

--- rotl.c
+++ rotl.c
@@ -7,10 +7,22 @@
  *
  * Return: void
  */
-void rotl(stack_t **stack, unsigned int line_number)
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 {
-	(void)line_number;
+	stack_t *tmp = *head, *aux;
 
-	if (*stack)
-		*stack = (*stack)->next;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
 }
